167ad988267ddc82a93b2af9e96ab5b3e7098e53
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-02-15 18:21:27 +0700

    add accordion

--- js/main.js
+++ js/main.js
@@ -23,6 +23,18 @@ window.onload = function () {
             x.className = 'b-page-nav'
         }
     }
+    var accodion = document.getElementsByClassName('b-btn-accordion');
+    for (i = 0; i < accodion.length; i++) {
+        accodion[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 }
 
 wow = new WOW(
