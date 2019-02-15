2a35bd97ebe915c654c7404b1ae8e926f9f4096a
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-02-15 10:18:27 +0700

    upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+window.onload = function () {
+    window.onscroll = function () {
+        scrollSticky();
+    }
+    var header = document.getElementById("b-page-header");
+    var sticky = header.offsetTop;
+    function scrollSticky() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    }
+}
+
 wow = new WOW(
     {
         animateClass: 'animated',
@@ -8,4 +23,14 @@ wow = new WOW(
     }
 );
 wow.init();
-
+jQuery(document).ready(function ($) {
+    "use strict";
+    // loading page
+    var loaderPage = function () {
+        $('.b-page-loader').fadeOut('slow');
+    }
+    // Document on load.
+    $(document).ready(function () {
+        loaderPage();
+    });
+});
